c54267e01cc13b74099f946e09ec8cccc769aa82
CHUN YAN NGAMCHAIWASANA <chunyanjimmy@gmail.com>
2026-01-28 18:31:57 +0000

    Initial commit

---
+++ app/+not-found.tsx
@@ -0,0 +1,33 @@
+import { View, StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+      <View style={styles.container}>
+        <Link href="/" style={styles.button}>
+          Go Back
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'left',
+  },
+
+  button: {
+    fontSize: 40,
+    marginLeft: 50,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
+
